365d7a7fad9f75e0363621082e35d56c6c044680
Saumya Srivastava <63807014+saumyasrivastava038@users.noreply.github.com>
2021-01-18 16:47:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 
+var port = process.env.PORT || 3000;
+
 const homeStartingContent = "Welcome to DAILY JOURNAL. You can use this application to track your day life. This application is built by Saumya Srivastava. Hope you like it. ";
 const aboutContent = "This application is build by using JavaScript and Nodejs.";
 const contactContent = "Hi, My name is Saumya Srivastava. I'm a web developer with a passion for full stack development and design. I'm currently a third year student at International Institute for Special Education pursuing a degree in Master of Computer Application. I aspire toward a career that will allow me to channel my creativity through crafting beautiful software and engaging experiences. Whether you have an idea for a project or just want to chat, feel free to shoot me an email!";
@@ -75,6 +77,6 @@ app.get("/contact", function(req, res){
 });
 
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
